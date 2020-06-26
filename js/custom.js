c2f4f7317d910a33be954f5bcbbb2ae7d9f01f8d
grammerpro <vardhanchess@gmail.com>
2020-06-26 19:39:26 +0530

    added contact form pageclip link

--- js/custom.js
+++ js/custom.js
@@ -117,17 +117,12 @@
                 method: form.attr("method"),
                 data: form.serialize(),
                 success: function(result) {
-                    if (result == "success") {
+
                         $(".form-inputs").css("display", "none");
                         $(".box p").css("display", "none");
                         $(".contactform").find(".output_message").addClass("success");
                         $(".output_message").text("Message Sent!");
-                    } else {
-                        $(".tabs-container").css("height", "440px");
-
-                        $(".contactform").find(".output_message").addClass("error");
-                        $(".output_message").text("Error Sending!");
-                    }
+                    
                 }
             });
 
