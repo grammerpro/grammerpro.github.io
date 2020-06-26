9b68a3bd232135cc4c50d876da35c09120960d9e
grammerpro <vardhanchess@gmail.com>
2020-06-26 19:11:29 +0530

    added contact form php link

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
-$subject = $_POST['subject'] . ' AagileCues Contact Form';
-$to = 'interior@aagilecues.com'; //Recipient's E-mail
+$subject = $_POST['subject'] . ' Vardhan website Contact Form';
+$to = 'vardhanchess@gmail.com'; //Recipient's E-mail
 
 $headers = 'MIME-Version: 1.0' . "rn";
 $headers .= "From: " . $_POST['email'] . "rn"; // Sender's E-mail
@@ -8,13 +8,12 @@ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "rn";
 
 $message .= 'Name: ' . $_POST['name'] . "\n";
 $message .= 'Email: ' . $_POST['email'] . "\n";
-$message .= 'Phone: ' . $_POST['phone'] . "\n";
 $message .= 'Message: ' . $_POST['message'];
 
 if (@mail($to, $subject, $message, $headers))
 {
     // Transfer the value 'sent' to ajax function for showing success message.
-    echo 'sent';
+    echo 'success';
 }
 else
 {
