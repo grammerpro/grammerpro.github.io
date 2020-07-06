0dfd70c779c1fdd13bae12e87cf83fb0002c1645
grammerpro <vardhanchess@gmail.com>
2020-07-06 15:17:24 +0530

    added sub website for testing and learning

--- js/custom.js
+++ js/custom.js
@@ -117,18 +117,16 @@
                 method: form.attr("method"),
                 data: form.serialize(),
                 success: function(result) {
-                    if (result == "success1") {
-
-						$(".tabs-container").css("height", "440px");
-
-						$(".contactform").find(".output_message").addClass("error");
-						$(".output_message").text("Error Sending!");
+                    if (result == "success") {
+                        $(".form-inputs").css("display", "none");
+                        $(".box p").css("display", "none");
+                        $(".contactform").find(".output_message").addClass("success");
+                        $(".output_message").text("Message Sent!");
                     } else {
-						$(".form-inputs").css("display", "none");
-						$(".box p").css("display", "none");
-						$(".contactform").find(".output_message").addClass("success");
-						$(".output_message").text("Message Sent!");
+                        $(".tabs-container").css("height", "440px");
 
+                        $(".contactform").find(".output_message").addClass("error");
+                        $(".output_message").text("Error Sending!");
                     }
                 }
             });
